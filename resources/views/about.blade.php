063dc715b55f7f85db0e38abd073033ef709f5ef
sigitariprasetyo <sigitari111@gmail.com>
2021-10-17 16:51:52 +0700

    init laravel, routers and views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css" />
+  <title>SAP Blog | About</title>
+</head>
+
+<body>
+  <h2>Halaman About</h2>
+  <h3><?= $name ?></h3>
+  <h4><?= $email ?></h4>
+  <img src="img/<?= $image ?>" alt="img" width="200px" />
+</body>
+
+</html>
