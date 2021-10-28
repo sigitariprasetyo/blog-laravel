6c76a65c7aa6df06919d6f1a95e63f86143c0c22
sigitariprasetyo <sigitari111@gmail.com>
2021-10-28 08:46:42 +0700

    add delete post

--- app/Http/Controllers/DashboardPostsController.php
+++ app/Http/Controllers/DashboardPostsController.php
@@ -102,7 +102,9 @@ class DashboardPostsController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has beed deleted!');
     }
 
     public function checkSlug(Request $request)
